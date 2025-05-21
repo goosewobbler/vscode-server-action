c8ea4266b8f2c849889a2246a11e83ad989c0faa
Sam Maister <goosewobbler@protonmail.com>
2025-05-21 20:47:54 +0100

    chore: fix type

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,22 +1,30 @@
 /// <reference types="vitest" />
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
-    test: {
-        include: ['test/**/*.test.ts'],
-        /**
-         * not to ESM ported packages
-         */
-        exclude: [
-            'build', '.idea', '.git', '.cache',
-            '**/node_modules/**', '__mocks__'
-        ],
-        coverage: {
-            enabled: true,
-            lines: 90,
-            functions: 95,
-            branches: 50,
-            statements: 90
-        }
-    }
+  test: {
+    include: ['test/**/*.test.ts'],
+    /**
+     * not to ESM ported packages
+     */
+    exclude: [
+      'dist',
+      'build',
+      '.idea',
+      '.git',
+      '.cache',
+      '**/node_modules/**',
+      '__mocks__',
+    ],
+    coverage: {
+      provider: 'v8',
+      enabled: true,
+      thresholds: {
+        lines: 80,
+        functions: 60,
+        branches: 50,
+        statements: 80,
+      }
+    },
+  },
 })
